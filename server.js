e5d40cb9dc82f26d4310e4ae8380ed7da4db649b
ninjaheatingandcooling <info@ninjaheatingandcooling.com>
2026-01-16 10:43:25 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ app.get("/", (req, res) => {
   res.status(200).send("Ninja AI Receptionist is running 🥷");
 });
 
-// SMS webhook (Twilio will call this later)
+// SMS webhook (Twilio will call this)
 app.post("/sms", (req, res) => {
+  console.log("INBOUND /sms HIT ✅");
+  console.log("Body:", req.body);
   res.status(200).send("OK");
 });
 
